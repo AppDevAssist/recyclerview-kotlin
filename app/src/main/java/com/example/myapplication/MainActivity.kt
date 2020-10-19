da2c22f856935926b97aa7297207574a6bd23c9e
n-k-d <n.k.d.assist@gmail.com>
2020-10-19 08:05:36 +0530

    updated view

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.models.Property
+import com.example.myapplication.network.Api
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     val data = arrayOf<String>("One", "Two", "three", "four","One", "Two", "three", "four","One", "Two", "three", "four","One", "Two", "three", "four","One", "Two", "three", "four","One", "Two", "three", "four","One", "Two", "three", "four")
@@ -17,14 +22,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         manager = LinearLayoutManager(this)
+        getAllData()
+    }
 
-        myAdapter = MyAdapter(data)
+    fun getAllData(){
+        Api.retrofitService.getAllData().enqueue(object: Callback<List<Property>>{
+            override fun onResponse(
+                call: Call<List<Property>>,
+                response: Response<List<Property>>
+            ) {
+                if(response.isSuccessful){
+                    recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply{
+                        myAdapter = MyAdapter(response.body()!!)
+                        layoutManager = manager
+                        adapter = myAdapter
+                    }
+                }
+            }
 
-
-        recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply{
-            layoutManager = manager
-            adapter = myAdapter
-        }
-
+            override fun onFailure(call: Call<List<Property>>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
     }
 }
--- app/src/main/java/com/example/myapplication/MyAdapter.kt
+++ app/src/main/java/com/example/myapplication/MyAdapter.kt
@@ -3,16 +3,29 @@ package com.example.myapplication
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.myapplication.models.Property
+import org.w3c.dom.Text
 
-class MyAdapter(private val data: Array<String>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>()  {
+class MyAdapter(private val data: List<Property>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>()  {
 
     class MyViewHolder(val view: View): RecyclerView.ViewHolder(view){
 
-        fun bind(text: String){
+        fun bind(property: Property){
 //            val tv = view.findViewById<TextView>(R.id.list_tv)
 //            tv.text = text
+            val title = view.findViewById<TextView>(R.id.tvTitle)
+            val imageView = view.findViewById<ImageView>(R.id.imageView)
+            val description = view.findViewById<TextView>(R.id.tvDescription)
+
+            title.text = property.title
+            description.text = property.description
+
+            Glide.with(view.context).load(property.image).centerCrop().into(imageView)
+
         }
     }
 
