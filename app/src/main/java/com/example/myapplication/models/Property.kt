60dcdca2a9f7b105d4106b3fe7ab785af2f50a15
nkaushikd <nandan6kaushik@gmail.com>
2020-10-19 08:05:36 +0530

    updated view

---
+++ app/src/main/java/com/example/myapplication/models/Property.kt
@@ -0,0 +1,3 @@
+package com.example.myapplication.models
+
+data class Property(val id: Int, val title: String, val description: String, val image: String)
---
+++ app/src/main/java/com/example/myapplication/network/ApiService.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication.network
+
+import com.example.myapplication.models.Property
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "http://simple-node-app-nkd.herokuapp.com"
+
+private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+private val retrofit = Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create(moshi)).baseUrl(BASE_URL).build()
+
+interface ApiService{
+
+    @GET(".")
+    fun getAllData(): Call<List<Property>>
+
+}
+
+object Api {
+    val retrofitService: ApiService by lazy{retrofit.create(ApiService::class.java)}
+}
