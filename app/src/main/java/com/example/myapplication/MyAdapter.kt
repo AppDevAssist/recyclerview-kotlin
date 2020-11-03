e070426cec80fea9a352135e6e02f779ea4a065a
n-k-d <n.k.d.assist@gmail.com>
2020-11-03 09:35:44 +0530

    Added delete button

--- app/src/main/java/com/example/myapplication/MyAdapter.kt
+++ app/src/main/java/com/example/myapplication/MyAdapter.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -10,21 +11,25 @@ import com.bumptech.glide.Glide
 import com.example.myapplication.models.Property
 import org.w3c.dom.Text
 
-class MyAdapter(private val data: List<Property>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>()  {
+class MyAdapter(private val data: List<Property>, val onClickDelete: (Int) -> Unit) : RecyclerView.Adapter<MyAdapter.MyViewHolder>()  {
 
-    class MyViewHolder(val view: View): RecyclerView.ViewHolder(view){
+    private var listData: MutableList<Property> = data as MutableList<Property>
+    inner class MyViewHolder(val view: View): RecyclerView.ViewHolder(view){
 
-        fun bind(property: Property){
+        fun bind(property: Property, index: Int){
 //            val tv = view.findViewById<TextView>(R.id.list_tv)
 //            tv.text = text
             val title = view.findViewById<TextView>(R.id.tvTitle)
             val imageView = view.findViewById<ImageView>(R.id.imageView)
             val description = view.findViewById<TextView>(R.id.tvDescription)
+            val button = view.findViewById<Button>(R.id.button)
 
             title.text = property.title
             description.text = property.description
 
             Glide.with(view.context).load(property.image).centerCrop().into(imageView)
+
+            button.setOnClickListener{deleteItem(index)}
 
         }
     }
@@ -35,12 +40,21 @@ class MyAdapter(private val data: List<Property>) : RecyclerView.Adapter<MyAdapt
     }
 
     override fun getItemCount(): Int {
-        return data.size
+        return listData.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.bind(data[position])
+        holder.bind(listData[position], position)
     }
 
+    fun deleteItem(index: Int){
+        listData.removeAt(index)
+        notifyDataSetChanged()
+    }
+
+    fun setItems(items: List<Property>){
+//        listData = items
+//        notifyDataSetChanged()
+    }
 
 }
