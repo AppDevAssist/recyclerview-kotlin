b596b2f5415ee22e90c744b603c5b5cc62cf31fb
n-k-d <n.k.d.assist@gmail.com>
2020-11-28 13:12:46 +0530

    Added swipe to refresh

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.myapplication.models.Property
 import com.example.myapplication.network.Api
 import retrofit2.Call
@@ -16,12 +17,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var manager: RecyclerView.LayoutManager
     private lateinit var myAdapter: MyAdapter
+    private lateinit var swipeRefresh: SwipeRefreshLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         manager = LinearLayoutManager(this)
+        swipeRefresh = findViewById(R.id.swipeRefresh)
+
+        swipeRefresh.setOnRefreshListener {
+            getAllData()
+        }
         getAllData()
     }
 
@@ -31,6 +38,9 @@ class MainActivity : AppCompatActivity() {
                 call: Call<List<Property>>,
                 response: Response<List<Property>>
             ) {
+                if(swipeRefresh.isRefreshing){
+                    swipeRefresh.isRefreshing = false
+                }
                 if(response.isSuccessful){
                     recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply{
                         data = response.body() as MutableList<Property>
